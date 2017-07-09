beb31c3b7b6c63005169a4e414e74d7e6a1c1540
edan-att <eb578m@att.com>
2017-07-09 10:05:19 +0300

    initial ionic commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,38 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AddReviewPage } from '../pages/add-review/add-review'
+import { ReviewsProvider } from '../providers/reviews/reviews';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AddReviewPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AddReviewPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ReviewsProvider,
+    AuthServiceProvider
   ]
 })
 export class AppModule {}
